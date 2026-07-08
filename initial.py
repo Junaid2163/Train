f5e4fa8f7ea840df965fac2606086e8039148be2
Junaid Ahamed <junaidahamed2163@gmail.com>
2026-07-08 11:57:02 +0400

    for loop

--- initial.py
+++ initial.py
@@ -1,5 +1,2 @@
-secret_word = "OpenAI"
-guess =""
-while guess != secret_word:
-    guess = input("Enter the guess: ")
-print("Congratulations! You've guessed the secret word.")
+for letter in "JUNAID":
+    print(letter)
