d7fa0395781020c068d62aa40ab766302a8f76f5
Junaid Ahamed <junaidahamed7779@gmail.com>
2026-07-08 13:33:23 +0400

    number grid

--- initial.py
+++ initial.py
@@ -1,7 +1,7 @@
-def raise_to_power(base_num, pow_num):
-    result = 1
-    for index in range(pow_num):
-        result= result * base_num
-    return result
-    
-print(raise_to_power(3,9))
+number_grid=[
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [0]
+]
+print(number_grid[3][0])  # Output: 0
