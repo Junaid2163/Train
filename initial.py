0a475fca0caed5779505331a442ccfa602241f53
Junaid Ahamed <junaidahamed7779@gmail.com>
2026-07-08 13:11:34 +0400

    iteration of loops

--- initial.py
+++ initial.py
@@ -1,3 +1,12 @@
-friends = ["Alice", "Bob", "Charlie", "David", "Eve"]
-for index in range(len(friends)):
-    print(friends[index])
+friends =["Alice", "Bob", "Charlie", "David", "Eve"]
+for index in range(5):
+    if index == 0:
+        print("first iteration")
+    elif index == 1:
+        print("second iteration")   
+    elif index == 2:            
+        print("third iteration")
+    elif index == 3:
+        print("fourth iteration")
+else:
+    print(" No iteration")
