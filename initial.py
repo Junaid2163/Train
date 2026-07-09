103727f404c8b36e4b0112cf606b1f0e4be67389
Junaid Ahamed <junaidahamed7779@gmail.com>
2026-07-09 14:09:31 +0400

    building a translator

--- initial.py
+++ initial.py
@@ -1,9 +1,13 @@
-number_grid=[
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [0]
-]
-for row in number_grid:
-    for col in row:
-        print(col)
+def translate (phrase):
+    translation =""
+    for letter in phrase:
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation = translation + "G"
+            else:
+                translation = translation + "g"
+        else:
+            translation = translation + letter
+    return translation
+
+print(translate(input("Enter a phrase: ")))
